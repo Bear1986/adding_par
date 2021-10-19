c94fafec0301bce76272233f061743682fd9dcb5
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2021-10-19 07:51:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+x_name = input("What is your name?: ")
+x_location = input("Where are you from?: ")
+
+def x (name,location):
+  print()
+  print(f"Hi {name}!")
+  print(f"whats the whether like in {location}?")
+
+x(x_name,x_location)
