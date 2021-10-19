21d2aac1d740ee7fc9a2c5b283320d584e49f7e7
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2021-10-19 08:05:04 +0000

    forgot to add a main func

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-x_name = input("What is your name?: ")
-x_location = input("Where are you from?: ")
 
 def x (name,location):
   print()
   print(f"Hi {name}!")
   print(f"whats the whether like in {location}?")
 
-x(x_name,x_location)
+def main():
+  x_name = input("What is your name?: ")
+  x_location = input("Where are you from?: ")
+  x(x_name,x_location)
+
+main()
